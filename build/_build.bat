49aee0c5e684fb8fae77da4a51a04bb9d412f315
Anthony Do <anthonyd3002@gmail.com>
2026-03-11 10:35:28 -0700

    adding to gitignore

--- build/_build.bat
+++ build/_build.bat
@@ -1,2 +1,2 @@
 set PATH=%PATH%;C:\msys64\ucrt64\bin
-g++ -std=c++20 -I../include ../src/*.cpp ../lib/*.a -lshlwapi -o TCScheduler
+g++ -g -std=c++20 -I../include ../src/*.cpp ../lib/*.a -lshlwapi -o TCScheduler
