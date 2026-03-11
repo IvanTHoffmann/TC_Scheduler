65edbc8666b7d900835325cda3435c5e2546fee1
IvanTHoffmann <55362684+IvanTHoffmann@users.noreply.github.com>
2026-03-11 10:35:02 -0700

    Handled an example button event

--- src/DemoEventListener.cpp
+++ src/DemoEventListener.cpp
@@ -4,6 +4,9 @@
 #include <RmlUi/Core/Elements/ElementFormControl.h>
 #include <RmlUi/Core/Elements/ElementFormControlSelect.h>
 #include <RmlUi_Backend.h>
+#include <iostream>
+
+using namespace std;
 
 DemoEventListener::DemoEventListener(const Rml::String& value, Rml::Element* element, DemoWindow* demo_window) :
 	value(value), element(element), demo_window(demo_window)
@@ -11,7 +14,12 @@ DemoEventListener::DemoEventListener(const Rml::String& value, Rml::Element* ele
 
 void DemoEventListener::ProcessEvent(Rml::Event& event)
 {
-	
+	if (value == "test_button")
+	{
+		cout << "clicked test button" << endl;
+
+		event.StopPropagation();
+	}
 }
 
 void DemoEventListener::OnDetach(Rml::Element* /*element*/)
