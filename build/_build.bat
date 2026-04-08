c906b10fb60149714c0a6eefad998c373fd5ff1f
IvanTHoffmann <55362684+IvanTHoffmann@users.noreply.github.com>
2026-04-07 22:16:32 -0700

    Changed build to use precompiled third-party library units

--- build/_build.bat
+++ build/_build.bat
@@ -1,4 +1,4 @@
 ::set PATH=%PATH%;C:\msys64\ucrt64\bin
-g++ -g -std=c++20 -I../include ../src/*.cpp ../lib/*.a -lshlwapi -o TCScheduler
+g++ -g -std=c++20 -I../include ../src/*.cpp ../src/thirdparty/*.o ../lib/*.a -lshlwapi -o TCScheduler
 
 TCScheduler.exe
